0f618d899789cd90eeb021e8a644346b7c137efa
joseCalfueque <j.calfueque02@ufromail.cl>
2025-04-19 11:12:25 -0400

    agrego una prueba de la clase de la IA , aun en proceso , probabemente se cambiara por otra idea para usar IA en el sistema

---
+++ src/Jugador.java
@@ -0,0 +1,17 @@
+public class Jugador {
+    private String nombre;
+    private String color;
+
+    public Jugador(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
